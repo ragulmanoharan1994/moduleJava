5f50184e8dee7f43271376b3da105c76d2cde068
Ragul <Ragul@LAPTOP-9B33V2BJ>
2022-02-03 18:56:27 +0530

    Initial Commit

---
+++ com.javafrm/src/com/javafrm/HelloPrint.java
@@ -0,0 +1,8 @@
+package com.javafrm;
+
+public class HelloPrint {
+	public String printHello() {
+		return "Welcome to Eclipse using Java 11";
+	}
+
+}
---
+++ com.javafrm/src/module-info.java
@@ -0,0 +1,3 @@
+module com.javafrm {
+	exports com.javafrm;
+}
